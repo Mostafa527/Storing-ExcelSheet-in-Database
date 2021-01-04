889bb2dfbe7fe6ce42f48dc100b48f8958e036ad
Mostafa.Net <mostafakhaled327@yahoo.com>
2021-01-04 13:12:00 +0530

    “OpenFileDialog”

--- ExcelSheetDB/src/excelsheetdb/ExcelSheetDB.java
+++ ExcelSheetDB/src/excelsheetdb/ExcelSheetDB.java
@@ -1,21 +1,48 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package excelsheetdb;
 
-/**
- *
- * @author Mostafa.Net
- */
+package exceltest;
+
+import java.io.File;
+import java.io.FileFilter;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.logging.Logger;
+import javax.swing.JFileChooser;
+import javax.swing.JOptionPane;
+import javax.swing.filechooser.FileNameExtensionFilter;
+
+
 public class ExcelSheetDB {
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
+ 
+    public static void main(String[] args) throws Exception {
+        String name_="",Address_="",phone_="";
+        int price_=0,quantity_=0;
+ 
+        ArrayList<String>client_name=new ArrayList<>();
+        ArrayList<String>phone=new ArrayList<>();
+            ArrayList<String>headers=new ArrayList<>();
+        ArrayList<Integer>prod_price=new ArrayList<>();
+        ArrayList<Integer>quantity=new ArrayList<>();
+        ArrayList<String>Address=new ArrayList<>();
+        
+        JFileChooser openfilechooser=new JFileChooser();
+        openfilechooser.setDialogTitle("Open File");
+        openfilechooser.removeChoosableFileFilter(openfilechooser.getFileFilter());
+        FileNameExtensionFilter filter=new FileNameExtensionFilter("Excel File (.xlsx)", "xlsx");
+        openfilechooser.setFileFilter(filter);
+        
+       
+        
+        }
+        
+        
+        
+        
     }
     
 }
